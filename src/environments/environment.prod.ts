42b71a17601b7f5fdb8f2ec448bd46cb607e83d4
Raúl González <raul.gomo@gmail.com>
2018-05-11 15:04:01 +0200

    últimos detalles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDYxEG8YbhH-IJm-uBOoCAd3ib08azSr5g",
+    apiKey: "",
     authDomain: "workflow-getronics.firebaseapp.com",
     databaseURL: "https://workflow-getronics.firebaseio.com",
     projectId: "workflow-getronics",
     storageBucket: "",
-    messagingSenderId: "641865235768"
+    messagingSenderId: ""
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDYxEG8YbhH-IJm-uBOoCAd3ib08azSr5g",
+    apiKey: "",
     authDomain: "workflow-getronics.firebaseapp.com",
     databaseURL: "https://workflow-getronics.firebaseio.com",
     projectId: "workflow-getronics",
     storageBucket: "",
-    messagingSenderId: "641865235768"
+    messagingSenderId: ""
   }
 };
